5a373dfc4a73af00552684bc0a7716f94acf7cc3
minghu6 <a19678zy@163.com>
2017-12-14 19:05:21 +0800

    rename git repo

--- setup.py
+++ setup.py
@@ -10,17 +10,20 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
     README = readme.read()
 
+
 def find_version():
     here = os.path.abspath(os.path.dirname(__file__))
     there = os.path.join(here, 'color', '__init__.py')
 
     version_file = codecs.open(there, 'r').read()
-    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", version_file, re.M)
+    version_match = re.search(
+        r"^__version__ = ['\"]([^'\"]*)['\"]", version_file, re.M)
     if version_match:
         return version_match.group(1)
 
     else:
         raise RuntimeError("Unable to find version string.")
+
 
 __version__ = find_version()
 
@@ -32,7 +35,7 @@ setup(
     license='BSD License',
     description='A color print wrapper',
     long_description=README,
-    url='https://github.com/minghu6/color_print',
+    url='https://github.com/minghu6/color-print',
     author='minghu6',
     author_email='a19678zy@163.com',
     classifiers=[
